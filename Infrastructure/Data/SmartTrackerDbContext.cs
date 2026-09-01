3f2b1382d059393f9747a7392ec2cb2540a7e3a0
Esenciak <karol.jablonski00@gmail.com>
2026-09-01 14:04:29 +0200

    feat(api): add PostgreSQL connection string and db context setup
    -dodaje PosgreSQL connection i je ustawiam

--- Infrastructure/Data/SmartTrackerDbContext.cs
+++ Infrastructure/Data/SmartTrackerDbContext.cs
@@ -5,7 +5,14 @@ namespace Infrastructure.Data
 {
 	public class SmartTrackerDbContext : DbContext
 	{
+		public SmartTrackerDbContext(DbContextOptions<SmartTrackerDbContext> options) : base(options)
+		{
+
+		}
+
 		public DbSet<Game> Games { get; set; }
 		public DbSet<Achievement> Achievements { get; set; }
 	}
+
+
 }
