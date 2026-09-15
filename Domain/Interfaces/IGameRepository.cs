b7bbeffa313beb2ae9fd6acc28fd6b9bf2d5c9d0
Esenciak <karol.jablonski00@gmail.com>
2026-09-15 22:02:59 +0200

    Add IGameRepository and GameRepository; bump EF Core
    Introduce IGameRepository (GetAllGamesAsync, AddAsync) and a concrete GameRepository that uses SmartTrackerDbContext to AddAsync and return all games via EF Core. Also bump Microsoft.EntityFrameworkCore to 10.0.12 in Api and Infrastructure projects and add Microsoft.OpenApi to the Api project.

---
+++ Domain/Interfaces/IGameRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Interfaces
+{
+	public interface IGameRepository
+	{
+		Task<IEnumerable<Game>> GetAllGamesAsync();
+		Task AddAsync(Game game);
+	}
+}
---
+++ Infrastructure/Repositories/GameRepository.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Data;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+	public class GameRepository : IGameRepository
+	{
+		private readonly SmartTrackerDbContext _context;
+
+		public GameRepository(SmartTrackerDbContext context)
+		{
+			_context = context;
+		}
+
+
+		public async Task AddAsync(Game game)
+		{
+			//throw new NotImplementedException();
+			await _context.Games.AddAsync(game);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task<IEnumerable<Game>> GetAllGamesAsync()
+		{
+			//throw new NotImplementedException();
+
+			return await _context.Games.ToListAsync();
+		}
+
+
+
+	}
+}
